8849f9f51a62a911eea733f2640e215aa748265b
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-04-03 09:49:41 -0400

    starting

--- build.sh
+++ build.sh
@@ -3,19 +3,25 @@
 WORK_DIR=$(mktemp -d) &&
     REPO_DIR=$(mktemp -d) &&
     git -C ${REPO_DIR} clone git@github.com:desertedscorpion/hollowmoon &&
-    git -C ${WORK_DIR} clone git@github.com:desertedscorpion/navyavenue.git &&
-    git -C ${WORK_DIR}/navyavenue checkout tags/0.0.1 &&
-    make --directory ${WORK_DIR}/navyavenue rebuild/emory-0.0.0-0.0.1 VERSION=0.0.0 &&
-    cp ${WORK_DIR}/silverfoot/rebuild/emory-0.0.0-0.0.1.x86_64.rpm ${REPO_DIR} &&
-#    git -C ${WORK_DIR} clone git@github.com:desertedscorpion/silverfoot.git &&
-#    git -C ${WORK_DIR}/silverfoot checkout tags/0.0.0 &&
-#    make --directory ${WORK_DIR}/silverfoot rebuild/jenkins-client-0.0.0-0.0.0 VERSION=0.0.0 &&
-#    cp ${WORK_DIR}/silverfoot/rebuild/jenkins-client-0.0.0-0.0.0.x86_64.rpm ${REPO_DIR} &&
+    function build_it(){
+	ORGANIZATION=${1} &&
+	    REPOSITORY=${2} &&
+	    VERSION=${3} &&
+	    RELEASE=${4} &&
+	    NAME=${5} &&
+	    git -C ${WORK_DIR} clone git@github.com:${ORGANIZATION}/${REPOSITORY}.git &&
+	    git -C ${WORK_DIR}/${REPOSITORY} checkout tags/${RELEASE} &&
+	    echo make --directory ${WORK_DIR}/${REPOSITORY} rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm VERSION=${VERSION} &&
+	    make --directory ${WORK_DIR}/${REPOSITORY} rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm VERSION=${VERSION} &&
+	    cp ${WORK_DIR}/${REPOSITORY}/rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm ${REPO_DIR}/hollowmoon &&
+	    git -C ${REPO_DIR}/hollowmoon add ${NAME}-${VERSION}-${RELEASE}.x86_64.rpm &&
+	    git -C ${REPO_DIR}/hollowmoon commit -m "Added ${NAME}-${VERSION}-${RELEASE}" &&
+	    true
+    } &&
+    build_it desertedscorpion navyavenue 0.0.1 0.0.4 luckygamma &&
     cd ${REPO_DIR}/hollowmoon &&
-    createrepo --pretty ${REPO_DIR} &&
-#    git -C ${REPO_DIR}/hollowmoon add jenkins-client-0.0.0-0.0.0.x86_64.rpm &&
-    git -C ${REPO_DIR}/hollowmoon add repodata.xml &&
-    git -C ${REPO_DIR}/hollowmoon commit -m "Added Files" &&
+    createrepo --pretty ${REPO_DIR}/hollowmoon &&
+    git -C ${REPO_DIR}/hollowmoon add repodata &&
     git -C ${REPO_DIR}/hollowmoon push origin master &&
     true
     
