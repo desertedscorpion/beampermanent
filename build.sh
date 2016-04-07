305ab93477b85a7145d2ec197ad53f26c494ecd5
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-04-07 12:15:16 -0400

    adding a lock

--- build.sh
+++ build.sh
@@ -15,42 +15,47 @@ WORK_DIR=$(dirname ${0}) &&
 	    VERSION_REPOSITORY=${4} &&
 	    NAME=${5} &&
 	    RELEASE_DIR=${WORK_DIR}/build/release &&
-	    mkdir --parents ${RELEASE_DIR} &&
-	    VERSION_DIR=${WORK_DIR}/build/version &&
-	    mkdir --parents ${VERSION_DIR} &&
-	    if [[ ! -d ${RELEASE_DIR}/${RELEASE_REPOSITORY} ]]
-	    then
-		git -C ${RELEASE_DIR} clone git@github.com:${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}.git &&
-		    true
-	    fi &&
-	    if [[ ! -d ${VERSION_DIR}/${VERSION_REPOSITORY} ]]
-	    then
-		git -C ${VERSION_DIR} clone git@github.com:${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}.git &&
-		    true
-	    fi &&
-	    git -C ${RELEASE_DIR}/${RELEASE_REPOSITORY} tag | while read RELEASE
-	    do
-		git -C ${VERSION_DIR}/${VERSION_REPOSITORY} tag | while read VERSION
-		do
-		    if [[ ! -f ${REPO_DIR}/hollowmoon/${NAME}-${VERSION}-${RELEASE}.x86-64.rpm ]] && [[ ! -f ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure ]]
+	    (
+		flock --exclusive 9
+		mkdir --parents ${RELEASE_DIR} &&
+		    VERSION_DIR=${WORK_DIR}/build/version &&
+		    mkdir --parents ${VERSION_DIR} &&
+		    if [[ ! -d ${RELEASE_DIR}/${RELEASE_REPOSITORY} ]]
 		    then
-			git -C ${RELEASE_DIR}/${RELEASE_REPOSITORY} checkout tags/${RELEASE} &&
-			    (
-				(
-				    make --directory ${RELEASE_DIR}/${RELEASE_REPOSITORY} rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm VERSION=${VERSION} &&
-					cp ${RELEASE_DIR}/${RELEASE_REPOSITORY}/rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm ${REPO_DIR}/hollowmoon &&
-					git -C ${REPO_DIR}/hollowmoon add ${NAME}-${VERSION}-${RELEASE}.x86_64.rpm &&
-					true
-				) || (
-				    touch ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure &&
-					true
-				)
-			    ) &&
+			git -C ${RELEASE_DIR} clone git@github.com:${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}.git &&
 			    true
-		    fi
-		done &&
+		    fi &&
+		    if [[ ! -d ${VERSION_DIR}/${VERSION_REPOSITORY} ]]
+		    then
+			git -C ${VERSION_DIR} clone git@github.com:${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}.git &&
+			    true
+		    fi &&
+		    git -C ${RELEASE_DIR}/${RELEASE_REPOSITORY} tag | while read RELEASE
+		    do
+			git -C ${VERSION_DIR}/${VERSION_REPOSITORY} tag | while read VERSION
+			do
+			    if [[ ! -f ${REPO_DIR}/hollowmoon/${NAME}-${VERSION}-${RELEASE}.x86-64.rpm ]] && [[ ! -f ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure ]]
+			    then
+				git -C ${RELEASE_DIR}/${RELEASE_REPOSITORY} checkout tags/${RELEASE} &&
+				    (
+					(
+					    make --directory ${RELEASE_DIR}/${RELEASE_REPOSITORY} rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm VERSION=${VERSION} &&
+						cp ${RELEASE_DIR}/${RELEASE_REPOSITORY}/rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm ${REPO_DIR}/hollowmoon &&
+						git -C ${REPO_DIR}/hollowmoon add ${NAME}-${VERSION}-${RELEASE}.x86_64.rpm &&
+						true
+					) || (
+					    touch ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure &&
+						true
+					)
+				    ) &&
+				    true
+			    fi
+			done &&
+			    true
+		    done &&
+		    rm ${WORK_DIR}/${RELEASE_ORGANIZATION}-${RELEASE_REPOSITORY}-${VERSION_ORGANIZATION}-${VERSION_REPOSITORY}-${NAME}.lock
 		    true
-	    done &&
+	    ) 9> ${WORK_DIR}/${RELEASE_ORGANIZATION}-${RELEASE_REPOSITORY}-${VERSION_ORGANIZATION}-${VERSION_REPOSITORY}-${NAME}.lock &&
 	    true
     } &&
     build_it desertedscorpion navyavenue desertedscorpion alienmetaphor luckygamma &&
@@ -63,5 +68,5 @@ WORK_DIR=$(dirname ${0}) &&
     git -C ${REPO_DIR}/hollowmoon commit -am "beampermanent" &&
     git -C ${REPO_DIR}/hollowmoon push origin master &&
     true
-    
-    
+
+
