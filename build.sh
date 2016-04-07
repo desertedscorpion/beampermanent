dce1f2d1fd4f7031e62e41f6c49a95ff4b74eca4
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-04-07 06:50:19 -0400

    save some work by not redoing

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-REPO_DIR=$(mktemp -d) &&
+REPO_DIR=build/repo &&
+    mkdir --parents ${REPO_DIR} &&
     git -C ${REPO_DIR} clone git@github.com:desertedscorpion/hollowmoon &&
     function build_it(){
 	RELEASE_ORGANIZATION=${1} &&
@@ -8,15 +9,17 @@ REPO_DIR=$(mktemp -d) &&
 	    VERSION_ORGANIZATION=${3} &&
 	    VERSION_REPOSITORY=${4} &&
 	    NAME=${5} &&
-	    RELEASE_DIR=$(mktemp -d) &&
-	    VERSION_DIR=$(mktemp -d) &&
+	    RELEASE_DIR=build/release &&
+	    mkdir --parents ${RELEASE_DIR} &&
+	    VERSION_DIR=build/version &&
+	    mkdir --parents ${VERSION_DIR} &&
 	    git -C ${RELEASE_DIR} clone git@github.com:${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}.git &&
 	    git -C ${VERSION_DIR} clone git@github.com:${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}.git &&
 	    git -C ${RELEASE_DIR}/${RELEASE_REPOSITORY} tag | while read RELEASE
 	    do
 		git -C ${VERSION_DIR}/${VERSION_REPOSITORY} tag | while read VERSION
 		do
-		    if [ ! -f ${REPO_DIR}/hollowmoon/${NAME}-${VERSION}-${RELEASE}.x86-64.rpm ]
+		    if [[ ! -f ${REPO_DIR}/hollowmoon/${NAME}-${VERSION}-${RELEASE}.x86-64.rpm ]] && [[ ! -f ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure ]]
 		    then
 			git -C ${RELEASE_DIR}/${RELEASE_REPOSITORY} checkout tags/${RELEASE} &&
 			    (
@@ -25,7 +28,10 @@ REPO_DIR=$(mktemp -d) &&
 					cp ${RELEASE_DIR}/${RELEASE_REPOSITORY}/rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm ${REPO_DIR}/hollowmoon &&
 					git -C ${REPO_DIR}/hollowmoon add ${NAME}-${VERSION}-${RELEASE}.x86_64.rpm &&
 					true
-				) || true
+				) || (
+				    touch ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure &&
+					true
+				)
 			    ) &&
 			    true
 		    fi
