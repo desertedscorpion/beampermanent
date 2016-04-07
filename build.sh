39a639c5b9e136dc116654c8309265ff92647fbc
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-04-07 12:26:55 -0400

    adding locks and work dir

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WORK_DIR=$(dirname ${0}) &&
+WORK_DIR=$(readlink -f $(dirname ${0})) &&
     REPO_DIR=${WORK_DIR}/build/repo &&
     mkdir --parents ${REPO_DIR} &&
     if [[ ! -d ${REPO_DIR}/hollowmoon ]]
@@ -14,10 +14,11 @@ WORK_DIR=$(dirname ${0}) &&
 	    VERSION_ORGANIZATION=${3} &&
 	    VERSION_REPOSITORY=${4} &&
 	    NAME=${5} &&
-	    RELEASE_DIR=${WORK_DIR}/build/release &&
 	    (
 		flock --exclusive 9
-		mkdir --parents ${RELEASE_DIR} &&
+		echo BUILDING ${RELEASE_ORGANIZATION} ${RELEASE_REPOSITORY} ${VERSION_ORGANIZATION} ${VERSION_REPOSITORY} ${NAME} &&
+		    RELEASE_DIR=${WORK_DIR}/build/release &&
+		    mkdir --parents ${RELEASE_DIR} &&
 		    VERSION_DIR=${WORK_DIR}/build/version &&
 		    mkdir --parents ${VERSION_DIR} &&
 		    if [[ ! -d ${RELEASE_DIR}/${RELEASE_REPOSITORY} ]]
@@ -34,22 +35,23 @@ WORK_DIR=$(dirname ${0}) &&
 		    do
 			git -C ${VERSION_DIR}/${VERSION_REPOSITORY} tag | while read VERSION
 			do
-			    if [[ ! -f ${REPO_DIR}/hollowmoon/${NAME}-${VERSION}-${RELEASE}.x86-64.rpm ]] && [[ ! -f ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure ]]
-			    then
-				git -C ${RELEASE_DIR}/${RELEASE_REPOSITORY} checkout tags/${RELEASE} &&
-				    (
+			    echo BUILDING RELEASE ${RELEASE} VERSION ${VERSION} &&
+				if [[ ! -f ${REPO_DIR}/hollowmoon/${NAME}-${VERSION}-${RELEASE}.x86-64.rpm ]] && [[ ! -f ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure ]]
+				then
+				    git -C ${RELEASE_DIR}/${RELEASE_REPOSITORY} checkout tags/${RELEASE} &&
 					(
-					    make --directory ${RELEASE_DIR}/${RELEASE_REPOSITORY} rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm VERSION=${VERSION} &&
-						cp ${RELEASE_DIR}/${RELEASE_REPOSITORY}/rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm ${REPO_DIR}/hollowmoon &&
-						git -C ${REPO_DIR}/hollowmoon add ${NAME}-${VERSION}-${RELEASE}.x86_64.rpm &&
-						true
-					) || (
-					    touch ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure &&
-						true
-					)
-				    ) &&
-				    true
-			    fi
+					    (
+						make --directory ${RELEASE_DIR}/${RELEASE_REPOSITORY} rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm VERSION=${VERSION} &&
+						    cp ${RELEASE_DIR}/${RELEASE_REPOSITORY}/rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm ${REPO_DIR}/hollowmoon &&
+						    git -C ${REPO_DIR}/hollowmoon add ${NAME}-${VERSION}-${RELEASE}.x86_64.rpm &&
+						    true
+					    ) || (
+						touch ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure &&
+						    true
+					    )
+					) &&
+					true
+				fi
 			done &&
 			    true
 		    done &&
