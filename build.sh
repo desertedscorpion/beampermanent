2dc1a6806aa1a6e4c96e9e94b0c65757702f9577
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-04-08 13:07:17 -0400

    changed

--- build.sh
+++ build.sh
@@ -1,80 +1,161 @@
 #!/bin/bash
 
-WORK_DIR=$(dirname ${0}) &&
-    REPO_DIR=${WORK_DIR}/build/repo &&
-    mkdir --parents ${REPO_DIR} &&
-    if [[ ! -d ${REPO_DIR}/hollowmoon ]]
+mkdir --parents build/{release,repo} &&
+    if [[ ! -d build ]]
     then
-	git -C ${REPO_DIR} clone git@github.com:desertedscorpion/hollowmoon &&
+	mkdir build &&
 	    true
     fi &&
-    git -C ${REPO_DIR}/hollowmoon pull --tags origin master &&
-    function build_it(){
+    if [[ ! -d build/repo ]]
+    then
+	mkdir build/repo &&
+	    true
+    fi &&
+    if [[ ! -d build/repo/hollowmoon ]]
+    then
+	git -C build/repo git@github.com:desertedscorpion/hollowmoon.git &&
+	    true
+    fi &&
+    git -C build/repo/hollowmoon pull --tags origin master &&
+    function artifacts(){
 	RELEASE_ORGANIZATION=${1} &&
 	    RELEASE_REPOSITORY=${2} &&
 	    VERSION_ORGANIZATION=${3} &&
 	    VERSION_REPOSITORY=${4} &&
 	    NAME=${5} &&
-	    (
-		flock --exclusive 9
-		echo BUILDING ${RELEASE_ORGANIZATION} ${RELEASE_REPOSITORY} ${VERSION_ORGANIZATION} ${VERSION_REPOSITORY} ${NAME} &&
-		    RELEASE_DIR=${WORK_DIR}/build/release &&
-		    mkdir --parents ${RELEASE_DIR} &&
-		    VERSION_DIR=${WORK_DIR}/build/version &&
-		    mkdir --parents ${VERSION_DIR} &&
-		    if [[ ! -d ${RELEASE_DIR}/${RELEASE_REPOSITORY} ]]
-		    then
-			git -C ${RELEASE_DIR} clone git@github.com:${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}.git &&
-			    true
-		    fi &&
-		    git -C ${RELEASE_DIR}/${RELEASE_REPOSITORY} pull --tags origin master &&
-		    if [[ ! -d ${VERSION_DIR}/${VERSION_REPOSITORY} ]]
-		    then
-			git -C ${VERSION_DIR} clone git@github.com:${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}.git &&
-			    true
-		    fi &&
-		    git -C ${VERSION_DIR}/${VERSION_REPOSITORY} pull --tags origin master &&
-		    git -C ${RELEASE_DIR}/${RELEASE_REPOSITORY} tag | while read RELEASE
-		    do
-			git -C ${VERSION_DIR}/${VERSION_REPOSITORY} tag | while read VERSION
-			do
-			    echo BUILDING RELEASE ${RELEASE} VERSION ${VERSION} &&
-				if [[ -f ${REPO_DIR}/hollowmoon/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm ]]
-				then
-				    echo WE HAVE ALREADY SUCCEEDED IN CREATING THIS RPM &&
-					true
-				elif [[ -f ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure ]]
-				then
-				    echo WE HAVE ALREADY FAILED IN CREATING THIS RPM &&
-					true
-				else
-				    git -C ${RELEASE_DIR}/${RELEASE_REPOSITORY} checkout tags/${RELEASE} &&
-					(
-					    (
-						make ${RELEASE_DIR}/${RELEASE_REPOSITORY}/rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm RELEASE_ORGANIZATION=${RELEASE_ORGANIZATION} RELEASE_REPOSITORY=${RELEASE_REPOSITORY} RELEASE=${RELEASE} VERSION_ORGANIZATION=${VERSION_ORGANIZATION} VERSION_REPOSITORY=${VERSION_REPOSITORY} VERSION=${VERSION} NAME=${NAME} &&
-						    cp ${RELEASE_DIR}/${RELEASE_REPOSITORY}/rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm ${REPO_DIR}/hollowmoon &&
-						    git -C ${REPO_DIR}/hollowmoon add ${NAME}-${VERSION}-${RELEASE}.x86_64.rpm &&
-						    true
-					    ) || (
-						mkdir ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure &&
-						    cp ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}.spec ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}.tar.gz ${RELEASE_DIR}/${RELEASE_REPOSITORY}/buildsrpm ${RELEASE_DIR}/${RELEASE_REPOSITORY}/rebuild ${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure${RELEASE_DIR}/${RELEASE_REPOSITORY}/${NAME}-${VERSION}-${RELEASE}.failure &&
-						    true
-					    )
-					) &&
-					true
-				fi
-			done &&
-			    true
-		    done &&
-		    rm ${HOME}/beampermanent.lock
+	    RELEASE=${6} &&
+	    VERSION=${7} &&
+	    if [[ -f build/repo/hollowmoon/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm ]]
+	    then
+		echo WE HAVE ALREADY SUCCEEDED IN BUILDING RELEASE_ORGANIZATION=${RELEASE_ORGANIZATION} RELEASE_REPOSITORY=${RELEASE_REPOSITORY} VERSION_ORGANIZATION=${VERSION_ORGANIZATION} VERSION_REPOSITORY=${VERSION_REPOSITORY} NAME=${NAME} RELEASE=${RELEASE} VERSION=${VERSION} &&
 		    true
-	    ) 9> ${HOME}/beampermanent.lock
+	    elif [[ -f build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/failure ]]
+	    then
+		echo WE HAVE ALREADY FAILED IN BUILDING RELEASE_ORGANIZATION=${RELEASE_ORGANIZATION} RELEASE_REPOSITORY=${RELEASE_REPOSITORY} VERSION_ORGANIZATION=${VERSION_ORGANIZATION} VERSION_REPOSITORY=${VERSION_REPOSITORY} NAME=${NAME} RELEASE=${RELEASE} VERSION=${VERSION} &&
+		    true
+	    else
+		sed -e "s#VERSION#${VERSION}#" -e "s#RELEASE#${RELEASE}#" -e "wbuild/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/${NAME}-${VERSION}.spec" build/release/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${NAME}.spec &&
+		    build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/${NAME}-${VERSION} &&
+		    git -C build/version/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY} archive --prefix ${NAME}/${VERSION} > build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/${NAME}-${VERSION}.tar &&
+		    gzip --to-stdout build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/${NAME}-${VERSION}.tar > build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/${NAME}-${VERSION}.tar.gz &&
+		    mkdir build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/buildsrpm &&
+		    mock buildsrpm --spec build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/${NAME}-${VERSION}.spec --sources build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/${NAME}-${VERSION}.tar.gz --resultdir build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/buildsrpm &&
+		    mkdir build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/rebuild &&
+		    mock rebuild build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/buildsrpm/${NAME}-${VERSION}-${RELEASE}.src.rpm --resultdir build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/rebuild &&
+		    cp build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION}/rebuild/${NAME}-${VERSION}-${RELEASE}.x86_64.rpm build/repo/hollowmoon &&
+		    git -C build/repo/hollowmoon add ${NAME}-${VERSION}-${RELEASE}.x86_64.rpm &&
+		    true
+    } &&
+    function version(){
+	RELEASE_ORGANIZATION=${1} &&
+	    RELEASE_REPOSITORY=${2} &&
+	    VERSION_ORGANIZATION=${3} &&
+	    VERSION_REPOSITORY=${4} &&
+	    NAME=${5} &&
+	    RELEASE=${6} &&
+	    VERSION=${7} &&
+	    git -C build/version/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY} checkout tags/${VERSION} &&
+	    if [[ ! -d build/artifacts/${RELEASE_ORGANIZATION} ]]
+	    then
+		mkdir --parents build/artifacts/${RELEASE_ORGANIZATION} &&
+		    true
+	    fi &&
+	    if [[ ! -d build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY} ]]
+	    then
+		mkdir --parents build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY} &&
+		    true
+	    fi &&
+	    if [[ ! -d build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE} ]]
+	    then
+		mkdir --parents build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE} &&
+		    true
+	    fi &&
+	    if [[ ! -d build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION} ]]
+	    then
+		mkdir --parents build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION} &&
+		    true
+	    fi &&
+	    if [[ ! -d build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY} ]]
+	    then
+		mkdir --parents build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY} &&
+		    true
+	    fi &&
+	    if [[ ! -d build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION} ]]
+	    then
+		mkdir --parents build/artifacts/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}/${RELEASE}/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY}/${VERSION} &&
+		    true
+	    fi &&
+	    (
+		artifacts ${RELEASE_ORGANIZATION} ${RELEASE_REPOSITORY} ${VERSION_ORGANIZATION} ${VERSION_REPOSITORY} ${NAME} ${RELEASE} ${VERSION} ||
+		    
+	    ) &&
 	    true
     } &&
-    build_it desertedscorpion navyavenue desertedscorpion alienmetaphor luckygamma &&
-    build_it desertedscorpion silverfoot desertedscorpion scatteredfinger jenkins-client &&
-    build_it desertedscorpion bigdrill desertedscorpion lostlocomotive jenkins-client-service &&
-    build_it desertedscorpion timelyvegetable desertedscorpion helplessmountain shinyalarm &&
+    function versions(){
+	RELEASE_ORGANIZATION=${1} &&
+	    RELEASE_REPOSITORY=${2} &&
+	    VERSION_ORGANIZATION=${3} &&
+	    VERSION_REPOSITORY=${4} &&
+	    NAME=${5} &&
+	    RELEASE=${6} &&
+	    if [[ ! -d build/version/${VERSION_ORGANIZATION} ]]
+	    then
+		mkdir --parents build/version/${VERSION_ORGANIZATION} &&
+		    true
+	    fi &&
+	    if [[ ! -d build/release/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY} ]]
+	    then
+		git -C build/release/${RELEASE_ORGANIZATION} clone git@github.com:${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}.git &&
+		    true
+	    fi &&
+	    git -C build/release/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY} pull --tags origin master &&
+	    git -C build/release/${VERSION_ORGANIZATION}/${VERSION_REPOSITORY} tag | while read VERSION
+	    do
+		version ${RELEASE_ORGANIZATION} ${RELEASE_REPOSITORY} ${VERSION_ORGANIZATION} ${VERSION_REPOSITORY} ${NAME} ${RELEASE} ${VERSION} &&
+		    true
+	    done &&
+	    true
+    } &&
+    function release(){
+	RELEASE_ORGANIZATION=${1} &&
+	    RELEASE_REPOSITORY=${2} &&
+	    VERSION_ORGANIZATION=${3} &&
+	    VERSION_REPOSITORY=${4} &&
+	    NAME=${5} &&
+	    RELEASE=${6} &&
+	    git -C build/release/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY} checkout tags/${RELEASE} &&
+	    versions ${RELEASE_ORGANIZATION} ${RELEASE_REPOSITORY} ${VERSION_ORGANIZATION} ${VERSION_REPOSITORY} ${NAME} ${RELEASE} &&
+	    true
+    } &&
+    function releases(){
+	RELEASE_ORGANIZATION=${1} &&
+	    RELEASE_REPOSITORY=${2} &&
+	    VERSION_ORGANIZATION=${3} &&
+	    VERSION_REPOSITORY=${4} &&
+	    NAME=${5} &&
+	    echo BUILDING RELEASES ${RELEASE_ORGANIZATION} ${RELEASE_REPOSITORY} &&
+	    if [[ ! -d build/release/${RELEASE_ORGANIZATION} ]]
+	    then
+		mkdir --parents build/release/${RELEASE_ORGANIZATION} &&
+		    true
+	    fi &&
+	    if [[ ! -d build/release/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY} ]]
+	    then
+		git -C build/release/${RELEASE_ORGANIZATION} clone git@github.com:${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY}.git &&
+		    true
+	    fi &&
+	    git -C build/release/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY} pull --tags origin master &&
+	    git -C build/release/${RELEASE_ORGANIZATION}/${RELEASE_REPOSITORY} tag | while read RELEASE
+	    do
+		release ${RELEASE_ORGANIZATION} ${RELEASE_REPOSITORY} ${VERSION_ORGANIZATION} ${VERSION_REPOSITORY} ${NAME} ${RELEASE} &&
+		    true
+	    done &&
+	    true
+    } &&
+    releases desertedscorpion navyavenue desertedscorpion alienmetaphor luckygamma &&
+    releases desertedscorpion silverfoot desertedscorpion scatteredfinger jenkins-client &&
+    releases desertedscorpion bigdrill desertedscorpion lostlocomotive jenkins-client-service &&
+    releases desertedscorpion timelyvegetable desertedscorpion helplessmountain shinyalarm &&
     createrepo --pretty ${REPO_DIR}/hollowmoon &&
     git -C ${REPO_DIR}/hollowmoon add repodata &&
     git -C ${REPO_DIR}/hollowmoon commit -am "beampermanent" &&
